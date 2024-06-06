4e758bc2ed15e784f600cd1ae2a0d8cab2c3d136
precious agbaragu <preciousagbaragu26@gmail.com>
2024-06-06 11:58:57 +0100

    added notification module

--- src/Amsweb/pages/ManagePeoplePage/managepeople.jsx
+++ src/Amsweb/pages/ManagePeoplePage/managepeople.jsx
@@ -1,10 +1,68 @@
-import React from "react";
-const managepeople = () => {
-	return (
-		<div>
-			<h1>mppage</h1>
-		</div>
-	);
-};
+import React, { useState } from "react";
+import "./managepeople.css";
+import HorizontalScroll from "../../components/HorizontalScroll/HorizontalScroll";
+import { CiSearch } from "react-icons/ci";
+import AddNewUser from "../AddNewUser/addNewUser";
+import { ToastContainer } from "react-toastify";
+// import menuDot from "../images/menu-dots";
 
-export default managepeople;
+function Managepeople() {
+  const [addNewUserPage, setAddNewUserPage] = useState(1);
+
+  const handleClick = () => {
+    setAddNewUserPage(2);
+  };
+
+  const items = [
+    "Item 1",
+    "Item 2",
+    "Item 3",
+    "Item 4",
+    "Item 5",
+    "hyy",
+    "hyy",
+    "hyy",
+  ];
+
+  return (
+    <>
+      <ToastContainer />
+      <div className="manage-people-container">
+        {addNewUserPage && addNewUserPage === 1 && (
+          <>
+            <header className="manage-people-header">
+              <h1>Administrators</h1>
+            </header>
+            <div className="scroll-child">
+              <HorizontalScroll items={items} />
+            </div>
+            <div className="info-and-search">
+              <div className="info-and-search-content">
+                <h2>All Users</h2>
+              </div>
+              <div className="info-and-search-content2">
+                <form className="info-and-search-searchbar">
+                  <input type="text" className="input" placeholder=" search" />
+                  <CiSearch id="search-icon" />
+                </form>
+                <div>
+                  <button className="add-user-button" onClick={handleClick}>
+                    Add new User
+                  </button>
+                </div>
+              </div>
+            </div>
+          </>
+        )}
+
+        {addNewUserPage && addNewUserPage == 2 && (
+          <>
+            <AddNewUser />
+          </>
+        )}
+      </div>
+    </>
+  );
+}
+
+export default Managepeople;
